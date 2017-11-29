d9d23b3905ddb31c627df5943faec742d57ebc7e
Vyacheslav Lapin <Vyachesl@vLapin.ru>
2017-11-29 15:29:36 +0300

    POJO intro - 2

--- src/Book.java
+++ src/Book.java
@@ -2,6 +2,7 @@
 //@AllArgsConstructor
 //@NoArgsConstructor
 public class Book {
+    private static final Book masterBook = new Book("Master of the rings");
     private String title;
 
     public Book() {
@@ -11,11 +12,25 @@ public class Book {
         this.title = title;
     }
 
+    public static void main(String... args) {
+        System.out.println(masterBook);
+//        masterBook = new Book("Мама мыла раму!");
+        masterBook.setTitle("Мама мыла раму!");
+        System.out.println(masterBook);
+    }
+
     public String getTitle() {
         return title;
     }
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                '}';
     }
 }
